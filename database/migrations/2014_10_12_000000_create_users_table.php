4452d24328d6973d057c9fc9c39bae3aa7a160d6
alfaz86 <hidecode403@gmail.com>
2024-07-17 18:26:23 +0700

    edit enum role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', [User::ROLE])->default(User::USER);
+            $table->enum('role', [User::getRoleKeys()])->default(User::USER);
             $table->rememberToken();
             $table->timestamps();
         });
